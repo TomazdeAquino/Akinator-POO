51c57063e3e50625ddc576ef1b6c1767b1b0c80b
Murilo Luciano <murilo.luciano@mail.usf.edu.br>
2023-11-06 21:27:09 -0300

    Criando ambiente de desenvolvimento no Net Beans

---
+++ Akinator-POO/src/main/java/com/mycompany/akinator/poo/AkinatorPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.akinator.poo;
+
+/**
+ *
+ * @author Murilo
+ */
+public class AkinatorPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
