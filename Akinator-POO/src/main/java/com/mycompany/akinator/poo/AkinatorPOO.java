a67a8cae22c1a40db5d1f9a0ea6608c33d91573d
Bruno Bilhar Karaziack <54771269+bruno-bk@users.noreply.github.com>
2023-11-09 23:42:50 -0300

    Executando o jogo a partir do main

--- Akinator-POO/src/main/java/com/mycompany/akinator/poo/AkinatorPOO.java
+++ Akinator-POO/src/main/java/com/mycompany/akinator/poo/AkinatorPOO.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.akinator.poo;
 
-/**
- *
- * @author Murilo
- */
 public class AkinatorPOO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Jogo jogo1 = new Jogo();
+        jogo1.jogar();
     }
 }
