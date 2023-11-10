858e344e407b2512514848cb310f35928e403931
TomazdeAquino <149121224+TomazdeAquino@users.noreply.github.com>
2023-11-09 21:39:22 -0300

    Update Perguntas.java
    O script imprime a pergunta e alternativas e retorna as resposta do usuário.

--- Akinator-POO/src/main/java/com/mycompany/akinator/poo/Perguntas.java
+++ Akinator-POO/src/main/java/com/mycompany/akinator/poo/Perguntas.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.akinator.poo;
 
-/**
- *
- * @author Murilo
- */
+import java.util.Scanner;
+
 public class Perguntas {
-    
+  private String enunciado;
+  private String[] alternativas;
+
+  public Perguntas(String enunciado, String[] alternativas) {
+    this.enunciado = enunciado;
+    this.alternativas = alternativas;
+  }
+
+  public String fazerPergunta() {
+      System.out.println(this.enunciado);
+      for (int i = 0; i < alternativas.length; i++) {
+          System.out.println(i + 1 + ". " + alternativas[i]);
+      }
+
+      Scanner scanner = new Scanner(System.in);
+      int resposta = scanner.nextInt();
+      scanner.nextLine(); // Limpa o buffer de entrada
+
+      if (resposta >= 1 && resposta <= alternativas.length) {
+          return alternativas[resposta - 1];
+      } else {
+          return "Opção inválida";
+      }
+  }
+
+  public String[] getAlternativas() {
+    return alternativas;
+  }
 }
